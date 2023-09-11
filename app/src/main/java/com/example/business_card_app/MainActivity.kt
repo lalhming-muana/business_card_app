8c1d5535083e9849e81007ef2db21b5b8fd1b005
Muana <lalhming.muana1982@gmail.com>
2023-09-11 22:47:17 +0530

    last commit before bed

--- app/src/main/java/com/example/business_card_app/MainActivity.kt
+++ app/src/main/java/com/example/business_card_app/MainActivity.kt
@@ -9,7 +9,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.business_card_app.ui.theme.Business_Card_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BusinessCard()
                 }
             }
         }
@@ -30,17 +39,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCard() {
+    val androidLem = painterResource(R.drawable.android_logo)
+    Column (Modifier.fillMaxSize()
+
+    ) {
+
+        Column(Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = androidLem,
+                contentDescription = null,
+                modifier = Modifier.weight(1f),
+            )
+
+            Text(
+                text = "Hello ",
+                fontWeight = FontWeight.Bold,
+                fontSize = 40.sp,
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Justify,
+                )
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Business_Card_AppTheme {
-        Greeting("Android")
+        BusinessCard()
     }
 }
