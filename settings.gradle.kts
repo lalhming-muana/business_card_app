3b4bbb7770c7d6f81f45694e73b36f09d8e5f329
lhmuana <edd_vai@yahoo.co.in>
2023-09-11 18:03:54 +0530

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Business_Card_App"
+include(":app")
+ 
